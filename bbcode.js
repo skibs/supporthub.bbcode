e5a3354cf73da6ade57abc09ae48ab8971ca1566
Charmander <campersander@gmail.com>
2014-03-27 14:32:44 -0700

    Added auto-linking.

--- bbcode.js
+++ bbcode.js
@@ -41,11 +41,12 @@ var ICON_ONLY_LINK = 'ICON_ONLY_LINK';
 var USERNAME_ONLY_LINK = 'USERNAME_ONLY_LINK';
 var HORIZONTAL_RULE = 'HORIZONTAL_RULE';
 var LINE_BREAK = 'LINE_BREAK';
+var AUTOMATIC_LINK = 'AUTOMATIC_LINK';
 
 var CSS3_OPAQUE_COLOUR = /^(?:#?([\da-f]{3}|[\da-f]{6})|rgb\((?:(?:\s*(?:25[0-5]|2[0-4]\d|[01]?\d{1,2})\s*,){2}\s*(?:25[0-5]|2[0-4]\d|[01]?\d{1,2})\s*|(?:\s*(?:100|0?\d{1,2})%\s*,){2}\s*(?:100|0?\d{1,2})%\s*)\)|hsl\(\s*(?:180|1[0-7]\d|0?\d{1,2})\s*,\s*(?:100|0?\d{1,2})%\s*,\s*(?:100|0?\d{1,2})%\s*\)|black|silver|gr[ae]y|white|maroon|red|purple|fuchsia|green|lime|olive|yellow|navy|blue|teal|aqua|aliceblue|antiquewhite|aqua|aquamarine|azure|beige|bisque|blanchedalmond|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgr[ae]y|darkgreen|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategr[ae]y|darkturquoise|darkviolet|deeppink|deepskyblue|dimgr[ae]y|dodgerblue|firebrick|floralwhite|forestgreen|gainsboro|ghostwhite|gold|goldenrod|greenyellow|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgr[ae]y|lightgreen|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategr[ae]y|lightsteelblue|lightyellow|limegreen|linen|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|oldlace|olivedrab|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|skyblue|slateblue|slategr[ae]y|snow|springgreen|steelblue|tan|thistle|tomato|turquoise|violet|wheat|whitesmoke|yellowgreen)$/i;
 
 function tokenize(input) {
-	var TOKEN = /\[([bisu]|sub|sup|quote|left|center|right)\]|\[\/([bisu]|sub|sup|color|quote|left|center|right|url)\]|\[(color|quote|url)=(?:"([^"]+)"|(\S*?))\]|:(icon|link)([\w-]+):|:([\w-]+)icon:|(\r?\n?-{5,}\r?\n?)|(\r\n|[\r\n\u2028\u2029])|\((c|r|tm)\)|[^-\r\n\u2028\u2029([:]+|[\S\s]/gi;
+	var TOKEN = /\[([bisu]|sub|sup|quote|left|center|right)\]|\[\/([bisu]|sub|sup|color|quote|left|center|right|url)\]|\[(color|quote|url)=(?:"([^"]+)"|(\S*?))\]|:(icon|link)([\w-]+):|:([\w-]+)icon:|(\r?\n?-{5,}\r?\n?)|(\r\n|[\r\n\u2028\u2029])|\((c|r|tm)\)|(\bhttps?:\/\/(?:[^\s?!.[\]]|[?!.](?=\w))+)|[^-h\r\n\u2028\u2029([:]+|[\S\s]/gi;
 	var tokens = [];
 	var m;
 
@@ -59,6 +60,7 @@ function tokenize(input) {
 			m[8] !== undefined  ? { type: ICON_ONLY_LINK, username: m[8] } :
 			m[9] !== undefined  ? { type: HORIZONTAL_RULE } :
 			m[10] !== undefined ? { type: LINE_BREAK } :
+			m[12] !== undefined ? { type: AUTOMATIC_LINK } :
 			                      { type: TEXT };
 
 		token.text = m[11] === undefined ? m[0] : symbols[m[11].toLowerCase()];
@@ -227,6 +229,18 @@ function render(input) {
 					'<img src="/users/' + token.username + '/image">' +
 					'</a>'
 				);
+
+				break;
+
+			case AUTOMATIC_LINK:
+				var info = linkInfo(token.text);
+
+				if (info.allowed) {
+					// TODO: rel="nofollow noreferrer" on external links if URIs ever become private
+					push('<a href="' + escapeAttributeValue(token.text) + (info.internal ? '">' : '" rel="nofollow">') + escapeContent(token.text) + '</a>');
+				} else {
+					push(escapeContent(token.text));
+				}
 
 				break;
 
